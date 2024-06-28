7c3790508aeb60b4495c2a7994966e6bb60ecead
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-06-28 19:03:45 -0300

    Commit criando o consolidado e a dedução

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -15,20 +15,41 @@ public class Program {
 		Double salario;
 		Double ganhoCapital;
 		Double gastosMedicos;
+		Double gastosEducacionais;
 		Double imposto;
+		Double impostoCapital;
 		Double rendaPrestacaoServico;
 		
 		
 		System.out.print("Renda anual com salário: "); // Escrevendo uma mensagem na tela
-		salarioAnual = leia.nextDouble(); // Capturando via teclado o número double digitado
+		salarioAnual = leia.nextDouble(); // Capturando via teclado o próximo número double digitado
 		salario = salarioAnual / 12;
 		System.out.print("Renda anual com prestação de serviço: ");
 		rendaPrestacaoServico = leia.nextDouble();
 		System.out.print("Renda anual com ganho de capital: ");
 		ganhoCapital = leia.nextDouble();
+		
+		if(ganhoCapital > 0) {
+			impostoCapital = (ganhoCapital * 0.20);
+		} else {
+			impostoCapital = 0.00;
+		}
+		
 		System.out.print("Gastos médicos: ");
 		gastosMedicos = leia.nextDouble();
 		System.out.print("Gastos educacionais: ");
+		gastosEducacionais = leia.nextDouble();
+		
+		// Relátório
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA");
+		
+		System.out.print("CONSOLIDADO DE RENDA: \n");
+		System.out.printf("Imposto sobre salário: %.2f\n",salario);
+		System.out.printf("Imposto sobre serviços: %.2f\n",rendaPrestacaoServico);
+		System.out.printf("Imposto sobre ganho de capital: %.2f\n",impostoCapital);
+		
+		// Deduções
+		System.out.println("DEDUÇÕES:");
 		
 		
 		
